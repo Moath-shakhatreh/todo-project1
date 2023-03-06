a8ba0829ccb8bd1556f8827e2c34400db0be41c0
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-03-06 15:57:33 +0000

    functions

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-function askingQuestion (){
-    let livingInjordan = prompt("do you live in jordan ?");
-    if (livingInjordan=="")
-    {
-    livingInjordan="invalid";
-    };
-    let background = prompt("do you have any information about programming");
-    if(background==""){
-        background="invalid";
-    };
-    let loveProgramming = prompt("do you love programming ?");
-    if(loveProgramming==""){
-        loveProgramming="invalid";
-    };
-    let answers = [livingInjordan,background,loveProgramming];
-        for (let i=0; i<answers.length; i++ ){
-            console.log(answers[i])
-        }
-    }
-    
-function welcomingMessage (){
-    let answer = confirm("Do you want to skip welcoming message ? ");
-
-    if(answer == false && gender == "male"){
-        alert("Welcome Mr."+name) 
-    }
-    else if(answer == false && gender == "female"){
-        alert("Welcome Ms."+name)
-    } else if (answer == false){
-        alert("Welcome")
-    };
-    }
-
-        
-
+'use strict';
 var name= prompt("please inter your name");
 var gender = prompt("what is yout gender ? ");
 var userAge = prompt("please inter your age");
 
-if (userAge <= 0 ){
-    alert("you'r under the allowed age")
-    };
+function checkAge (){
+    if (userAge <= 0 ){
+        alert("you'r under the allowed age")
+        };
+    }
 
-askingQuestion ();    
+    let answers =[];
 
+function askQuestions (arr){
+    let livingInjordan = prompt("do you live in jordan ?");
+    let background = prompt("do you have any information about programming");
+    let loveProgramming = prompt("do you love programming ?");
+    arr.push(livingInjordan,background,loveProgramming);
+    
+    }
+
+function check(arr){
+        for (let i=0; i<arr.length; i++ ){
+           if (arr[i]==""){
+            arr[i]="invalid";
+           }
+    }
+}
+ 
+
+function showingInConsole(arr){
+    for (let i=0; i<arr.length; i++ ){
+        console.log(arr[i])
+}
+}
+
+    function welcomingMessage (){
+        let answer = confirm("Do you want to skip welcoming message ? ");
+    
+        if(answer == false && gender == "male"){
+            alert("Welcome Mr."+name) 
+        }
+        else if(answer == false && gender == "female"){
+            alert("Welcome Ms."+name)
+        } else if (answer == false){
+            alert("Welcome")
+        };
+        }
+
+
+checkAge();
+askQuestions(answers); 
+check(answers); 
+showingInConsole(answers);  
 welcomingMessage();
+
 
 
 
